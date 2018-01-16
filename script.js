3b94fd8cdd014889c0cd4512f462064038a52b88
1hk_star <1hk_star@naver.com>
2018-01-17 07:11:10 +0900

    update korean encoding problem

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     link.addEventListener('keypress', function() {
     	if( event.key == "Enter")
     	{
-    		var text = link.value;
+    		var text = encodeURIComponent(document.getElementById('search_google').value);
     		window.open("https://www.google.co.kr/search?source=hp&ei=XlleWujDOYG68QX4pajoAw&q="+text+"&oq=hello&gs_l=psy-ab.3..0l10.4605.5999.0.6169.9.7.0.0.0.0.93.485.6.7.0....0...1c.1.64.psy-ab..2.6.485.0..0i131k1j0i10k1.70.CkOXDPjrajY","_blank");
     	}
     });
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
     link.addEventListener('keypress', function() {
     	if( event.key == "Enter")
     	{
-    		var text = link.value;
+    		var text = encodeURIComponent(document.getElementById('search_naver').value);
     		window.open("https://search.naver.com/search.naver?query="+text,"_blank");
     	}
     });
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function() {
     link.addEventListener('keypress', function() {
     	if( event.key == "Enter")
     	{
-    		var text = link.value;
+    		var text = encodeURIComponent(document.getElementById('search_youtube').value);
     		window.open("https://www.youtube.com/results?search_query="+text,"_blank");
     	}
     });
