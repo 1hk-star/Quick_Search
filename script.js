f5c22d152c4a10a08ce016ece70dcdbbc7c6ab0d
1hk_star <1hk_star@naver.com>
2018-01-17 06:41:33 +0900

    search on google, naver, youtube and if clicked logo, move to site

---
+++ script.js
@@ -0,0 +1,57 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('search_google');
+    // onClick's logic below:
+    link.addEventListener('keypress', function() {
+    	if( event.key == "Enter")
+    	{
+    		var text = link.value;
+    		window.open("https://www.google.co.kr/search?source=hp&ei=XlleWujDOYG68QX4pajoAw&q="+text+"&oq=hello&gs_l=psy-ab.3..0l10.4605.5999.0.6169.9.7.0.0.0.0.93.485.6.7.0....0...1c.1.64.psy-ab..2.6.485.0..0i131k1j0i10k1.70.CkOXDPjrajY","_blank");
+    	}
+    });
+
+    var link = document.getElementById('img_google');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+    		window.open("https://www.google.co.kr","_blank");
+    });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('search_naver');
+    // onClick's logic below:
+    link.addEventListener('keypress', function() {
+    	if( event.key == "Enter")
+    	{
+    		var text = link.value;
+    		window.open("https://search.naver.com/search.naver?query="+text,"_blank");
+    	}
+    });
+
+    var link = document.getElementById('img_naver');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+    		window.open("https://www.naver.com","_blank");
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('search_youtube');
+    // onClick's logic below:
+    link.addEventListener('keypress', function() {
+    	if( event.key == "Enter")
+    	{
+    		var text = link.value;
+    		window.open("https://www.youtube.com/results?search_query="+text,"_blank");
+    	}
+    });
+
+    var link = document.getElementById('img_youtube');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+    		window.open("https://www.youtube.com","_blank");
+    });
+});
